e4e78979cacbac688ceee70b8971aa83f72e669a
Abdullahi Salaudeen <abdullahisalaudeen65@gmail.com>
2025-07-12 12:54:29 +0100

    add logo

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,7 +1,7 @@
 function Button({ url, children, linkClass }) {
   return (
     <button className="bg-neutral-700 px-2 py-1 md:px-4 md:py-1 hover:bg-neutral-500 cursor-pointer transition-colors ">
-      <a href={url} className={linkClass}>{children}</a>
+      <a href={url} className={linkClass} target="_blank" rel="noopener noreferrer">{children}</a>
     </button>
   );
 }
